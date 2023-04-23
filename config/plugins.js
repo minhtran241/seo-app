1c021a3cded1c57c2d8585699e30a215596599fc
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2023-04-23 10:05:36 -0400

    add some plugins

--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,17 @@ module.exports = ({ env }) => ({
     enabled: true,
     resolve: "./src/plugins/strapi-plugin-ckeditor",
   },
+  seo: {
+    enabled: true,
+  },
+  translate: {
+    enabled: false, // Turn on after getting the API key
+    config: {
+      // Add the name of your provider here (for example 'deepl' for strapi-provider-translate-deepl or the full package name)
+      // provider: "deepl",
+      // providerOptions: {
+      //   // Your provider might define some custom options like an apiKey
+      // },
+    },
+  },
 });
